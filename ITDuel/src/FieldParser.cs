59b635e866719c8dd24d017fb6cfc34e581cb041
Matvey Goff <matvey.goff@gmail.com>
2016-09-03 13:49:15 +0300

    Region model

---
+++ ITDuel/src/FieldParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nancy.FourColors
+{
+    public static class FieldParser
+    {
+        private static void ProcessCell(int[,] field, int row, int col, IList<Region> result)
+        {
+            if
+                ((row < 0) || (row >= field.GetLength(0))
+                || ((col < 0) || (col >= field.GetLength(1))))
+                return;
+        }
+
+        public static IList<Region> Parse(int[,] field)
+        {
+
+            var result = new List<Region>();
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+//                    ProcessCell(field, i - 1, )
+                }
+            }
+            return result;
+        }
+    }
+}
